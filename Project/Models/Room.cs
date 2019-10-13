a6724714fcc8f5f3c4e2ac0c92052fc1afefe72c
Katrina Fuller <trinafuller@att.net>
2019-10-13 10:59:47 -0600

    properly able to use the flashlight

--- Project/Models/Room.cs
+++ Project/Models/Room.cs
@@ -48,22 +48,24 @@ namespace ConsoleAdventure.Project.Models
     {
       string template = $"Room: {Name} \n {Description}\n";
 
-      foreach (var exit in Exits)
+
+
+      // if flashlight is on or in rooms 1 or 2, show where to go
+      if (_game.UsingFlashlight || _game.CurrentRoom.Name == "1" || _game.CurrentRoom.Name == "2")
       {
-        template += "\n \t" + exit.Key + " brings you to Room " + exit.Value.Name + Environment.NewLine;
+        foreach (var exit in Exits)
+        {
+          template += "\n \t" + exit.Key + " brings you to Room " + exit.Value.Name + Environment.NewLine;
+        }
+        foreach (var item in Items)
+        {
+          template += $"Item: \n{item.Name} \t {item.Description} \n";
+        }
       }
-
-
-      //if not rooms 1 or 2 and no flashlight is on, do now show directions on where you can go
-      if (!_game.CurrentRoom.Name == room1 || !_game.CurrentRoom.Name == room2)
+      else
       {
-
-      }
-
-
-      foreach (var item in Items)
-      {
-        template += $"Item: \n{item.Name} \t {item.Description} \n";
+        // flashlight is OFF and we are not in room 1 or 2 
+        template += $"It's really dark in here. You can't see where to go.\n";
       }
       return template;
     }
